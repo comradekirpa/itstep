4bba14b91854d836d3189516fa9c32447f61dca6
comradekirpa <77933656+comradekirpa@users.noreply.github.com>
2021-02-02 16:20:38 +0200

    Lesson3

---
+++ Lesson 3/screaming.py
@@ -0,0 +1,4 @@
+name = input("Enter your name ")
+
+print("Привет " + name)
+print("name")
---
+++ Lesson 3/variables.py
@@ -0,0 +1,20 @@
+current_year = 2021
+
+light_speed_in_vacuum = 299792.458
+
+quote = "\"Можливо все, неможливе просто потребує більше часу\".\nДен Браун"
+
+oceans = ["Pacifik", "Atlantic", "Indian", "Southern", "Arctic"]
+
+active = True
+
+naiming = ["i", "t", "s", "t", "e", "p"]
+
+school = {"students amount":["Neo", "Trinity", "Morpheus", "Mous", "Tank"], "subjects": ["Python", "Math", "English", "Kung-Fu",]}
+print(current_year)
+print(light_speed_in_vacuum)
+print(quote)
+print(oceans)
+print(active == True)
+print(naiming)
+print (school)
